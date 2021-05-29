4287e7683c2e305b3f4512d26434184986f5feb5
Leo <leo.cho1989@gmail.com>
2021-05-28 19:04:10 -0700

    third commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,31 @@ class User < ApplicationRecord
     after_initialize :ensure_session_token
 
     def reset_session_token!
-        
+        self.session_token = SecureRandom::urlsafe_base64
+        self.save!
+        self.session_token
+    end
+
+    def password
+        @password
+    end
+
+    def password=(password)
+        self.password_digest = BCrypt::Password.create(password)
+        @password = password
+    end
+
+    def is_password(password)
+        password_object = BCrypt::Password.new(self.password_digest)
+        password_object.is_password?(password)
+    end
+
+    def self.find_by_credentials(user_name,password)
+        user = User.find_by(user_name: user_name)
+        if user&& user.is_password?(password)
+            user
+        else
+            nil
+        end
     end
 end
